3fa8719adb010672d44e82f72fb272ec94782888
George Handyside-Lang <Georgehl060605@gmail.com>
2024-08-01 04:39:17 +0100

    Added eventListener

--- GRO development/script.js
+++ GRO development/script.js
@@ -1,1 +1,21 @@
-"use strict";
+
+// Get all elements with the class 'Services'
+const servicesButton = document.querySelectorAll('.Services');
+
+// get popup element
+const popupElement = document.getElementById('popup');
+const popupImage = popupElement.querySelector('img');
+
+servicesButton.addEventListener('mouseover',function(event){//uses event as argument to obtain data
+    // changes the css from display none to display block hence showing it 
+    popupElement.style.display = 'block';
+    popupImage.src = servicesButton.getAttribute('data-image');
+    // Position the popup near the button
+    const rect = servicesButton.getBoundingClientRect();
+    popupElement.style.left = `${rect.left + window.scrollX + rect.width / 2}px`;
+    popupElement.style.top = `${rect.top + window.scrollY}px`;
+  });
+  servicesButton.addEventListener('mouseout', function () {
+    // Hide the popup
+    popupElement.style.display = 'none';
+  });
